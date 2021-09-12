72b96da1041fcbc301c954492f635852183ac855
Erick Arévalo <kerick.fabricio@gmail.com>
2021-09-12 13:36:05 -0600

    Diseño home

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,10 +1,21 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
+import { Image, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-ionicons';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { HomeScreen } from '../views/home/HomeScreen';
 
-export const TabNavigator = ({setLoginState}) => {
+export const TabNavigator = ({ setLoginState }) => {
+
+    function LogoTitle() {
+        return (
+          <Image
+            style={{ width: 50, height: 50 }}
+            source={require('../assets/test_img/login.jpg')}
+          />
+        );
+      }
+
     const Tab = createBottomTabNavigator();
     return (
         <NavigationContainer>
@@ -14,29 +25,32 @@ export const TabNavigator = ({setLoginState}) => {
                         let iconName;
 
                         if (route.name === 'Inicio') {
-                            iconName = focused
-                                ? 'ios-information-circle'
-                                : 'ios-information-circle-outline';
-                        } else if (route.name === 'Settings') {
-                            iconName = focused ? 'ios-list-box' : 'ios-list';
+                            iconName = focused ? 'home' : 'home-outline';
+                        } else if (route.name === 'Buscar') {
+                            iconName = focused ? 'search' : 'search-outline';
+                        } else if (route.name === 'Mis compras') {
+                            iconName = focused ? 'book' : 'book-outline';
+                        } else if (route.name === 'Lista de deseos') {
+                            iconName = focused ? 'bookmarks' : 'bookmarks-outline';
+                        } else if (route.name === 'Configuración') {
+                            iconName = focused ? 'person-circle' : 'person-circle-outline';
                         }
+
                         // You can return any component that you like here!
                         return <Icon name={iconName} size={size} color={color} />;
                     },
-                    tabBarActiveTintColor: 'tomato',
+                    tabBarActiveTintColor: '#f04e98',
                     tabBarInactiveTintColor: 'gray',
                     tabBarShowLabel: false
-                })}
-
-            >
-{/*                 <Tab.Screen name="Home">
+                })}>
+                {/*                 <Tab.Screen name="Home">
                     {() => <HomeScreen setLoginState={setLoginState} />}
                 </Tab.Screen> */}
-                <Tab.Screen name="Inicio" component={HomeScreen} />
-                <Tab.Screen name="Búscar" component={HomeScreen} />
+                <Tab.Screen name="Inicio" options={{ headerTitle: props => <LogoTitle {...props} /> }} component={HomeScreen} />
+                <Tab.Screen name="Buscar" component={HomeScreen} />
                 <Tab.Screen name="Mis compras" component={HomeScreen} />
                 <Tab.Screen name="Lista de deseos" component={HomeScreen} />
-                <Tab.Screen name="Settings" component={HomeScreen} />
+                <Tab.Screen name="Configuración" component={HomeScreen} />
             </Tab.Navigator>
         </NavigationContainer>
     )
--- views/home/HomeScreen.js
+++ views/home/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View, Button } from 'react-native';
-
+// ..
 export const HomeScreen = ({ setLoginState }) => {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
