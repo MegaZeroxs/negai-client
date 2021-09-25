2284cb0a73b9c94e6ebd86632743769ebc695f32
Erick Arévalo <kerick.fabricio@gmail.com>
2021-09-25 14:43:24 -0600

    Diseño inicial home

--- views/home/HomeScreen.js
+++ views/home/HomeScreen.js
@@ -1,11 +1,52 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, SafeAreaView, ScrollView, StyleSheet, Image, Dimensions } from 'react-native';
 // ..
-export const HomeScreen = ({ setLoginState }) => {
+const win = Dimensions.get('window');
+const ratio = win.width / 541; //541 is actual image width
+
+const ItemCard = ({title, price}) => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#FFFFFF'}}>
-            <Text>Desloguearse</Text>
-            <Button onPress={setLoginState} title="Ir offline" />
+        <View style={styles.card_container}>
+            <Image style={styles.card_img} source={require('../../assets/covers/001/cover.jpg')} />
+            <Text>{title}</Text>
+            <Text>{price}</Text>
         </View>
+    );
+}
+
+export const HomeScreen = () => {
+    return (
+        <SafeAreaView>
+            <ScrollView style={styles.scroolview_container}>
+                <View style={styles.section_container}>
+                    <View style={styles.list_container}>
+                        <ItemCard title="Houseki no kuni - Vol 1" price="3.99" />
+                        <ItemCard title="Houseki no kuni - Vol 1" price="3.99" />
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    scroolview_container: {
+        backgroundColor: '#FFFFFF'
+    },
+    list_container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        padding: 10,
+        width: '100%',
+        alignItems: 'center',
+    },
+    card_container: {
+        flexBasis: '50%',
+        alignItems: 'center',
+    },
+    card_img: {
+        width: (win.width / 2.4),
+        height: 340 * ratio, //340 es la altura de los cover originales
+    }
+});
