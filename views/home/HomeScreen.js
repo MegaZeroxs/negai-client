15deeba5f01682a80212a72b78dc8e17483d6f67
Erick Arévalo <kerick.fabricio@gmail.com>
2021-09-25 15:53:45 -0600

    Cambio a modelo funcional y tarjetas en la home

--- views/home/HomeScreen.js
+++ views/home/HomeScreen.js
@@ -4,12 +4,12 @@ import { Text, View, Button, SafeAreaView, ScrollView, StyleSheet, Image, Dimens
 const win = Dimensions.get('window');
 const ratio = win.width / 541; //541 is actual image width
 
-const ItemCard = ({title, price}) => {
+const ItemCard = ({title, price, vol}) => {
     return (
         <View style={styles.card_container}>
             <Image style={styles.card_img} source={require('../../assets/covers/001/cover.jpg')} />
-            <Text>{title}</Text>
-            <Text>{price}</Text>
+            <Text style={styles.card_title} numberOfLines={1} style={{ width: '100%'}}>{title}</Text>
+            <Text style={styles.card_price}>Volumen {vol} - ${price}</Text>
         </View>
     );
 }
@@ -19,9 +19,10 @@ export const HomeScreen = () => {
         <SafeAreaView>
             <ScrollView style={styles.scroolview_container}>
                 <View style={styles.section_container}>
+                    <Text>Hola</Text>
                     <View style={styles.list_container}>
-                        <ItemCard title="Houseki no kuni - Vol 1" price="3.99" />
-                        <ItemCard title="Houseki no kuni - Vol 1" price="3.99" />
+                        <ItemCard title="Houseki no kuni" vol="1" price="14.99" />
+                        <ItemCard title="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" vol="11" price="14.99" />
                     </View>
                 </View>
             </ScrollView>
@@ -31,22 +32,33 @@ export const HomeScreen = () => {
 
 const styles = StyleSheet.create({
     scroolview_container: {
-        backgroundColor: '#FFFFFF'
+        backgroundColor: '#FFFFFF',
+    },
+    section_container:{
+        padding: 15,
+        paddingTop: 0
     },
     list_container: {
         flexDirection: 'row',
         flexWrap: 'wrap',
         justifyContent: 'space-between',
-        padding: 10,
         width: '100%',
-        alignItems: 'center',
     },
     card_container: {
-        flexBasis: '50%',
-        alignItems: 'center',
+        flexBasis: '48%',
     },
     card_img: {
-        width: (win.width / 2.4),
+        width: (win.width / 2.25),
         height: 340 * ratio, //340 es la altura de los cover originales
+    },
+    card_title:{
+        fontFamily: 'CreatoDisplay-Regular',
+        color: '#686868',
+        fontSize: 15
+    },
+    card_price:{
+        fontFamily: 'CreatoDisplay-Bold',
+        color: '#000',
+        fontSize: 14
     }
 });
