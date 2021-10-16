7dc42aa0dae5c750760f2a20d84b579779aac3c2
Erick Arévalo <kerick.fabricio@gmail.com>
2021-10-15 22:23:14 -0600

    Consulta actualizada new comics

--- views/home/components/ScrollCards.js
+++ views/home/components/ScrollCards.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View, ScrollView, Button } from 'react-native';
 import { styles } from '../../../assets/Styles';
-import { useFetchComicsHome } from '../../../hooks/useFetchComicsHome';
+import { useFetchComicsRecentHome } from '../../../hooks/useFetchComicsRecentHome';
 import { ItemScrollCard } from './ItemScrollCard';
 
 export const ScrollCards = ({ navigation, title }) => {
 
-    const {data:comics, loading } = useFetchComicsHome( );
+    const {data:comics, loading } = useFetchComicsRecentHome( );
     return (
         <View style={styles.section_container}>
             <Text style={styles.section_title}>{title}</Text>
